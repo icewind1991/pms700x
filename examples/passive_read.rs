1b2e24812600384cc241bd62dc6811e3d5708925
Robin Appelman <robin@icewind.nl>
2020-08-16 06:20:16 +0200

    sleep in passive read example

--- examples/passive_read.rs
+++ examples/passive_read.rs
@@ -3,11 +3,14 @@ use nb::block;
 use pms700x::Pms700X;
 use std::env::args;
 use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let port = args().skip(1).next().expect("No serial provided");
     let serial = Serial::open(&PathBuf::from(port)).unwrap();
 
     let mut pms = Pms700X::new(serial).into_passive().unwrap();
+    sleep(Duration::from_millis(50));
     dbg!(block!(pms.read()).unwrap());
 }
